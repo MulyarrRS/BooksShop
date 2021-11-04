3b6ad3cfb0e83a095a77675fbb3720b67e4782f2
22222 <1111>
2021-11-04 20:32:49 +0300

    Added BookModel

---
+++ Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksShop.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string NameBooK { get; set; }
+        public string AutorBook { get; set; }
+        public int YearPublication { get; set; }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksShop.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books {get; set;}
+        public BookContext (DbContextOptions<BookContext> options)
+            : base(options) 
+
+        {
+
+            Database.EnsureCreated();
+        }
+
+    }
+}
