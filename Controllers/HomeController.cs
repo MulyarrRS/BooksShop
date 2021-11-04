32194cf110ca15645cdbb5034fb35b7f69132295
22222 <1111>
2021-11-04 18:10:50 +0300

    Add Autentification

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,31 +7,27 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Authorization;
+
 namespace BooksShop.Controllers
 {
+  
+ 
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+            [Authorize]
+            public IActionResult Index()
+            {
+                if (User.Identity.IsAuthenticated) 
+                {
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+                    return Content(User.Identity.Name);
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+                }
+                
+                return Content("Не аутентифицирован");  
+            }
     }
+ 
 }
